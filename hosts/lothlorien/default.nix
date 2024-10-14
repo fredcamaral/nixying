d0a6579fcb5420c55bb4400c247c388ac1628aae
Fred Amaral <fred@fredamaral.com.br>
2024-10-14 19:23:15 -0300

    update

--- hosts/lothlorien/default.nix
+++ hosts/lothlorien/default.nix
@@ -8,6 +8,30 @@
     ./../../modules/core
   ];
 
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = Samba Server
+      server role = standalone server
+      log file = /var/log/samba/log.%m
+      max log size = 50
+      dns proxy = no
+      map to guest = bad user
+    '';
+    shares = {
+      public = {
+        path = "/media/ness";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
   hardware = {
     amdgpu.amdvlk = {
       enable = true;
